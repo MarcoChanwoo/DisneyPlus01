fc5b516a527bfedc61543fb736a07e5261665c1e
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-27 17:44:58 +0900

    updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,7 +18,7 @@ const Nav = () => {
   }, []);
 
   return (
-    <NavWrapper>
+    <NavWrapper show={show}>
       <Logo>
         <img
           alt="Disney Plus Logo"
@@ -38,7 +38,7 @@ const NavWrapper = styled.nav`
   left: 0;
   right: 0;
   height: 70px;
-  background-color: #090b13;
+  background-color: ${(props) => (props.show ? "#090b13" : "transparent")};
   display: flex;
   justify-content: space-between;
   align-items: center;
