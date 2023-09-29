a696729203d7f8d8a3562b028d355d9d9d862311
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-29 11:28:46 +0900

    updated

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,6 +1,16 @@
 import React from "react";
+import "./MovieModal.css";
 
-const MovieModal = () => {
+const MovieModal = ({
+  backdrop_path,
+  title,
+  overview,
+  name,
+  release_date,
+  first_air_date,
+  vote_average,
+  setModalOpen,
+}) => {
   return <div>MovieModal</div>;
 };
 
