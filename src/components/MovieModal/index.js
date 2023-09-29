c108f59168922693b0a1177856ac3e33c639c51c
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-29 11:37:06 +0900

    updated

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -11,7 +11,28 @@ const MovieModal = ({
   vote_average,
   setModalOpen,
 }) => {
-  return <div>MovieModal</div>;
+  return (
+    <div className="presentation" role="presentation">
+      <div className="wrapper-modal">
+        <span onClick={() => setModalOpen(false)} className="modal-close">
+          X
+        </span>
+        <img
+          className="modal_poster-img"
+          src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+          alt="modal-img"
+        />
+        <div className="modal_content">
+          <p className="modal_details">
+            <span className="modal_user_perc">100% for you</span>{" "}
+            {release_date ? release_date : first_air_date}
+          </p>
+
+          <h2 className="modal_title">{title ? title : name}</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieModal;
