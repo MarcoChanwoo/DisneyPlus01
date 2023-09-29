4507ef62a175446e6113c7ed9c535f193518c32d
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-29 11:53:06 +0900

    updated

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -14,23 +14,25 @@ const MovieModal = ({
   return (
     <div className="presentation" role="presentation">
       <div className="wrapper-modal">
-        <span onClick={() => setModalOpen(false)} className="modal-close">
-          X
-        </span>
-        <img
-          className="modal_poster-img"
-          src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
-          alt="modal-img"
-        />
-        <div className="modal_content">
-          <p className="modal_details">
-            <span className="modal_user_perc">100% for you</span>{" "}
-            {release_date ? release_date : first_air_date}
-          </p>
+        <div className="modal">
+          <span onClick={() => setModalOpen(false)} className="modal-close">
+            X
+          </span>
+          <img
+            className="modal_poster-img"
+            src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+            alt="modal-img"
+          />
+          <div className="modal_content">
+            <p className="modal_details">
+              <span className="modal_user_perc">100% for you</span>{" "}
+              {release_date ? release_date : first_air_date}
+            </p>
 
-          <h2 className="modal_title">{title ? title : name}</h2>
-          <p className="modal_overview">평점: {vote_average}</p>
-          <p className="modal_overview">{overview}</p>
+            <h2 className="modal_title">{title ? title : name}</h2>
+            <p className="modal_overview">평점: {vote_average}</p>
+            <p className="modal_overview">{overview}</p>
+          </div>
         </div>
       </div>
     </div>
