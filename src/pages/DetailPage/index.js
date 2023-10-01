e3867dab5d5805a4ce2f35b4eaa8d9ccb259e054
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-01 10:01:39 +0900

    updated

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -13,7 +13,18 @@ const DetailPage = () => {
     }
     fetchData();
   }, [movieId]);
-  return <div>detail</div>;
+
+  if (!movie) return null;
+
+  return (
+    <section>
+      <img
+        className="modal_poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt="img"
+      />
+    </section>
+  );
 };
 
 export default DetailPage;
