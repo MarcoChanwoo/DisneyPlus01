5363e764f6715200542152536bb8aa38c3f46571
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-02 18:06:54 +0900

    updated

--- src/components/Row.js
+++ src/components/Row.js
@@ -26,39 +26,15 @@ const Row = ({ title, id, fetchUrl }) => {
   return (
     <div>
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider_arrow-left">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-            }}
-          >
-            {"<"}
-          </span>
-        </div>
-        <div id={id} className="row_posters">
-          {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className="row_poster"
-              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-              alt={movie.name}
-              onClick={() => handleClick(movie)}
-            />
-          ))}
-        </div>
-        <div className="slider_arrow-right">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft += window.innerWidth - 80;
-            }}
-          >
-            {">"}
-          </span>
-        </div>
-      </div>
+      {movies.map((movie) => (
+        <img
+          key={movie.id}
+          className="row_poster"
+          src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+          alt={movie.name}
+          onClick={() => handleClick(movie)}
+        />
+      ))}
       {modalOpen && (
         <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
       )}
