01a3eb2e02f8b824bb6ef6bbe4fd11cb06283b82
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-04 14:42:41 +0900

    updated

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
 const LoginPage = () => {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <Content>
+        <Center>
+          <LogoOne />
+        </Center>
+      </Content>
+    </Container>
+  );
 };
 
 export default LoginPage;
+
+const Container = styled.section``;
+
+const Content = styled.div``;
+
+const Center = styled.div``;
+
+const LogoOne = styled.img``;
