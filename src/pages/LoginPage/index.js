d0f959f9624a60e7674560194c81ec26397c34f0
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-04 14:51:19 +0900

    updated

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -6,7 +6,13 @@ const LoginPage = () => {
     <Container>
       <Content>
         <Center>
-          <LogoOne />
+          <LogoOne src="/images/cta-logo-one.svg" alt="logo-one" />
+          <SignUpLink>지금 가입</SignUpLink>
+          <Description>
+            영화에 대한 프리미어 액세스를 얻으십시오. 디즈니 플러스 가격은 다음
+            주부터 1000원 인상됩니다.
+          </Description>
+          <LogoTwo src="images/cta-logo-two.svg" alt="logo-two" />
         </Center>
       </Content>
     </Container>
@@ -22,3 +28,9 @@ const Content = styled.div``;
 const Center = styled.div``;
 
 const LogoOne = styled.img``;
+
+const SignUpLink = styled.a``;
+
+const Description = styled.p``;
+
+const LogoTwo = styled.img``;
