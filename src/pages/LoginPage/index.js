8ae31eb13ed217de71fec89df90c2f331db62f25
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-04 15:02:50 +0900

    updated

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -58,7 +58,18 @@ const LogoOne = styled.img`
   width: 100%;
 `;
 
-const SignUpLink = styled.a``;
+const SignUpLink = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px;
+  border: 1px solid transparent;
+  border-radius: 4px;
+`;
 
 const Description = styled.p``;
 
