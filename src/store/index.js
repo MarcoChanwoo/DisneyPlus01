f02c34f82ed1b051c397283d0f92fe89ee8f3fbc
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-25 23:17:18 +0900

    updated

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,15 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
   user: userReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  Storage,
+};
 
 export const store = configureStore({
   reducer: {
